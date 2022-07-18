08fb6f45c6a97e17a5304886afaa419f0a909194
harshana <uwillfindharshana@gmail.com>
2022-07-18 21:22:10 +0530

    add socioeconomic page

---
+++ app/src/sim/socioeconomic.js
@@ -0,0 +1,12 @@
+function SocioEconomic(){
+
+    return (
+        <div>
+            <div className="results-page">
+                <h1>Socio Economic analysis</h1>
+            </div>
+        </div>
+    )
+}
+
+export default SocioEconomic;
